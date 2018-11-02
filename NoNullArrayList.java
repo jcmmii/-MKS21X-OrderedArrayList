b75de9ac8b7adea3700fd672ecc85f3d689c2e86
Justin Chen <jchen06@stuy.edu>
2018-11-02 10:14:07 -0400

    Functional NoNullArrayList

---
+++ NoNullArrayList.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class NoNullArrayList<T> extends ArrayList<T>{
+  public NoNullArrayList() {
+      super();
+  }
+
+  public NoNullArrayList(int startingCapacity) {
+    super(startingCapacity);
+  }
+
+  public void add(int index, T element) {
+    if (element == null) {
+      throw new IllegalArgumentException("Can't add null");
+    } else {
+      super.add(index, element);
+    }
+  }
+
+  public boolean add(T element) {
+    if (element == null) {
+      throw new IllegalArgumentException("Can't add null");
+    } else {
+      super.add(element);
+      return true;
+    }
+  }
+
+  public T set(int index, T element) {
+    if (element == null) {
+      throw new IllegalArgumentException("Can't set null");
+    } else {
+      super.set(index,element);
+    }
+    return element;
+  }
+
+}

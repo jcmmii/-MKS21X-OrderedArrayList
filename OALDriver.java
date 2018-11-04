a3ba99cc7f7559e7bcf268f07e97f5a121b20cb5
Justin Chen <jchen06@stuy.edu>
2018-11-04 15:48:33 -0500

    Final Driver

--- OALDriver.java
+++ OALDriver.java
@@ -1,28 +1,60 @@
 public class OALDriver {
   public static void main(String[] args) {
-    OrderedArrayList<Integer> A = new OrderedArrayList<Integer>();
-    A.add(5);
-    A.add(2);
-    A.add(20);
-    A.add(-4);
-    A.add(-19);
-    A.add(7);
-    A.add(13);
-    A.add(23);
-    System.out.println(A);
-    System.out.println(A.findLowest());
-    System.out.println(A.sort());
+    NoNullArrayList<Integer> A = new NoNullArrayList<Integer>();
+      System.out.println("\nThis is NoNullArrayList A: " + A);
+      try {
+        System.out.println("Trying to add null...");
+        A.add(null);
+      } catch (IllegalArgumentException e){
+        System.out.println("Caught catching null - you can't do that");
+      }
+      System.out.println("Adding 2,6,-10 to A");
+      A.add(2);
+      A.add(6);
+      A.add(-10);
+      System.out.println("A is " + A);
+      System.out.println("Setting index 1 to 3");
+      A.set(1,3);
+      System.out.println("A is " + A);
+      System.out.println("Adding 7 to index 1");
+      A.add(1,7);
+      System.out.println("A is " + A);
 
-    OrderedArrayList<String> B = new OrderedArrayList<String>();
-    B.add("");
-    B.add("");
-    B.add("");
-    System.out.println(B);
-    System.out.println(B.findLowest());
-    System.out.println(B.sort());
+    System.out.println("\n--------\n");
 
+    OrderedArrayList<Integer> B = new OrderedArrayList<Integer>();
+      System.out.println("This is OrderedArrayList B: " + B);
+      System.out.println("Adding 5,7,-9,3,-2 to B");
+      B.add(5);
+      B.add(7);
+      B.add(3);
+      B.add(-2);
+      B.add(-9);
+      System.out.println("B is " + B);
+      System.out.println("Trying to add 77 at index 2");
+      B.add(2,77);
+      System.out.println("B is " + B);
+      //automatically reorders it, can't add 77 at index 2
+      //since values behind it are smaller
+      try {
+        System.out.println("Trying to add null...");
+        B.add(null);
+      } catch (IllegalArgumentException e){
+        System.out.println("Caught catching null - you can't do that");
+      }
+      System.out.println("Set index 3 to 100 - removes 5, adds 100");
+      B.set(3,100);
+      System.out.println("B is " + B);
 
-    A.set(0,2);
-    System.out.println(A);
+    System.out.println("\n--------\n");
+
+    OrderedArrayList<String> C = new OrderedArrayList<String>();
+      System.out.println("This is OrderedArrayListC: " + C);
+      System.out.println("Adding strings hey, WHATS, UP, dude! to C");
+      C.add("hey");
+      C.add("WHATS");
+      C.add("UP");
+      C.add("dude!");
+      System.out.println("C is " + C);
   }
 }
